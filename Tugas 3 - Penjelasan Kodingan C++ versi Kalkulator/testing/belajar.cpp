b08c37c424f17dda8bba2928c17e829f4868bcbb
Ilham Taufiq <ilhamtaufiq859@gmail.com>
2022-06-21 08:17:13 +0700

    Bismillah

---
+++ Tugas 3 - Penjelasan Kodingan C++ versi Kalkulator/testing/belajar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int angka = 2;
+int angka2 = 3;
+int tambah = angka + angka2;
+string salam = "assalamualaikum ";
+string nama;
+
+int main(){
+	//cout<<"angka adalah : "<<angka<<endl;
+	//cout<<"angka2 adalah : "<<angka2<<endl;
+	//cout<<"hasil tambah : "<<tambah<<endl;
+	cout<<"Nama : ";
+	cin>>nama;
+	cout<<salam<<nama;
+}
+
