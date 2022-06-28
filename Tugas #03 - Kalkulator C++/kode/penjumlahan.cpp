cc131a24a524666417a6690cd8b74fac1345903c
Ilham Taufiq <ilhamtaufiq859@gmail.com>
2022-06-28 14:01:43 +0700

    update tugas

---
+++ Tugas #03 - Kalkulator C++/kode/penjumlahan.cpp
@@ -0,0 +1,27 @@
+// library C++
+#include <iostream>
+using namespace std;
+
+// function main(utama)
+int main(){
+
+	// inisiasi variable
+    int a, b, c;
+
+	// meminta input variable a
+    cout << "Inputkan nilai a: ";
+    cin >> a;
+    
+    // meminta input variable b
+    cout << "Inputkan nilai b: ";
+    cin >> b;
+
+    // menggunakan operator penjumlahan
+    c = a + b;
+    
+	// menampilkan hasil penjumlahan
+    cout << "Hasil a + b = " << c << endl;
+
+	// menutup program
+    return 0;
+}
